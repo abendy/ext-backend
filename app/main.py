64c9191209a1ac92eb8c8fc0cfb906e9c0932bb1
Allan Bendy <allan.bendy@gmail.com>
2019-02-22 13:43:58 +1100

    implements redis caching

--- app/main.py
+++ app/main.py
@@ -1,30 +1,40 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import redis
 import requests
 from readability import Document
 
 app = Flask(__name__)
 
+cache = redis.Redis(host='127.0.0.1', port=6379)
+
 @app.route('/')
-def newspaper():
-    url = request.args.get('page')
+def main():
+    url = request.args.get('url')
     title = ''
 
-    if url:
+    if not url:
+        return "?url=&lt;url&gt; pls"
+
+    else:
         try:
-            response = requests.get(url, timeout=10)
+            if cache.hexists('page', url):
+                rt = cache.hget('page', url).decode("utf-8")
+            else:
+                response = requests.get(url, timeout=15)
 
-            if response.ok:
-                page = Document(response.text)
-
-                title = page.title()
-                page = page.summary(True)
-
-                return render_template('home.html', title=title, page=page)
-
+                if response.ok:
+                    cache.hset('page', url, response.text)
+                    rt = response.text
         except:
-            print('Error processing URL: %s' % url)
+            return "Error processing URL: %s" % url
+
+    doc = Document(rt)
+    title = doc.title()
+    page = doc.summary(True)
+
+    return render_template('home.html', title=title, page=page)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
