7faf08fdf92dd8e8353fa435e808e926866f072a
Allan Bendy <allan.bendy@gmail.com>
2019-03-21 22:18:15 +1100

    adds basic ssl

--- app/main.py
+++ app/main.py
@@ -87,4 +87,4 @@ def delete():
     return "Deleted highlight: %s" % highlight_id
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True, ssl_context=('cert.pem', 'key.pem'))
