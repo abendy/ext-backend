18945c116609353ccf81708b27c7eb994bcd44eb
Allan Bendy <allan.bendy@gmail.com>
2019-03-22 00:58:35 +1100

    fix cors

--- app/main.py
+++ app/main.py
@@ -3,12 +3,14 @@ from flask import render_template
 from flask import request
 from flask import jsonify
 from flask import make_response
+from flask_cors import CORS
 from readability import Document
 import json
 import redis
 import requests
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 cache = redis.Redis(host='127.0.0.1', port=6379)
 
